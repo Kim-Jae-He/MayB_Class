198a75cc5960a9fbe8e5975b2d7ffd7907b59679
Kim-Jae-He <jgim78628@gmail.com>
2023-11-23 19:44:31 +0900

    11-23

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { users } from './user';
+import express from 'express';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-console.log('Users :', users);
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(8000, () => {
+  console.log('서버시작되었습니다');
+});
